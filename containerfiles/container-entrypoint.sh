1847f4bcc9b9792ea4d3fffc5368d3588c3a9c09
GTPE Student <tjungbau-redhat.com@clientvm.33fa.internal>
2019-12-17 14:27:33 +0000

    testing ...

--- containerfiles/container-entrypoint.sh
+++ containerfiles/container-entrypoint.sh
@@ -80,9 +80,9 @@ then
   counter=0
   for i in $( echo ${SERVICE_DEST}| sed -e 's/;/ /g') ; do
     if [[ $SERVICE_DEST == *":"* ]]; then
-      $SERVICE_PORT="" 
+      SERVICE_PORT="" 
     else 
-      $SERVICE_PORT=":80"
+      SERVICE_PORT=${SERVICE_DEST_PORT}
     fi
     #server_lines=${server_lines}$(echo -e server "${SERVICE_NAME}_"$(printf "%03i" "$counter") ${i}:${SERVICE_DEST_PORT} check \\\\\n)
     server_lines=${server_lines}$(echo -e server "${SERVICE_NAME}_"$(printf "%03i" "$counter") ${i}${SERVICE_PORT} check \\\\\n)
